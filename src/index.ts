f3a8ef5a70eb871793774e47cba0d3db99a7a929
Hamaz-Chihab <c_hamaz@estin.dz>
2024-02-07 20:16:36 +0100

    transform into typescript

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-const app = require("./server");
+import app from './server'
 app.listen(3000, () => {
   console.log("server is opened");
 });
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
-const express = require("express");
-
+import express from "express"
 const app = express();
 app.get("/", (req, res) => {
   console.log("hello from express");
   res.status(200);
   res.json({ message: "hello" });
 });
-module.exports = app;
+export default app 
