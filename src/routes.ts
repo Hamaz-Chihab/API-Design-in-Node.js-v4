f5578b887614a3ab1888fa65835e29fc464f7c05
Hamaz-Chihab <c_hamaz@estin.dz>
2024-02-08 20:46:09 +0100

    second day

---
+++ src/routes.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+
+const router = Router();
+//product routes
+router.get("/product", () => {});
+router.get("/product/:id", () => {});
+router.put("/product/:id", () => {});
+router.post("/product", () => {});
+router.delete("/product/:id", () => {});
+
+//update routes
+router.get("/update", () => {});
+router.get("/update/:id", () => {});
+router.put("/update/:id", () => {});
+router.post("/update", () => {});
+router.delete("/update/:id", () => {});
+
+//updatePoints routes
+router.get("/updatepoints   ", () => {});
+router.get("/updatepoints/:id", () => {});
+router.put("/updatepoints/:id", () => {});
+router.post("/updatepoints", () => {});
+router.delete("/updatepoints/:id", () => {});
