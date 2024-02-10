28e603860a49487495471b44a154f4e8c937f426
Hamaz-Chihab <c_hamaz@estin.dz>
2024-02-10 23:04:30 +0100

    part tow of the auth function

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,4 +1,4 @@
-import jwt from "jsonwebtoken ";
+import jwt from "jsonwebtoken";
 export const createJWT = (user) => {
   const token = jwt.sign(
     {
@@ -9,11 +9,27 @@ export const createJWT = (user) => {
   );
   return token;
 };
-export const protect = (req, res) => {
+export const protect = (req, res, next) => {
   const bearer = req.headers.authorization;
   if (!bearer) {
     res.status(401);
     res.json({ message: "not authorized" });
+    return;
+  }
+  const [, token] = bearer.split(" ");
+  if (!token) {
+    res.status(401);
+    res.json({ message: "not valide token" });
+    return;
+  }
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    next();
+  } catch (e) {
+    console.error(e);
+    res.status(401);
+    res.json({ message: "not valide Token" });
     return;
   }
 };
