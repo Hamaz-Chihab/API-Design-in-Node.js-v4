ae3804cb0d9268cda76d248d0a32a46f918d8c8f
Hamaz-Chihab <c_hamaz@estin.dz>
2024-03-20 03:23:53 +0100

    some comments

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,6 @@ import merge from "lodash.merge";
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 const stage = process.env.STAGE || "local";
 let envConfig;
-
 if (stage === "production") {
   envConfig = require("./prod").default;
 } else if (stage === "testing") {
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+//to use the env-var setup in config file
 import * as dotenv from "dotenv";
 dotenv.config();
 import app from "./server";
 import config from "./config";
-
+console.log(process.env);
 app.listen(config.port, () => {
   console.log(`hello on http://localhost:${config.port}`);
   console.log("the server is opened NOW");
